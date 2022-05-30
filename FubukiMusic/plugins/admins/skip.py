b6e98268ae207ecb1161c68c342ba7c302279a31
CoolGuyKisaki <93737399+CoolGuyKisaki@users.noreply.github.com>
2022-05-30 17:50:20 +0530

    Update skip.py

--- FubukiMusic/plugins/admins/skip.py
+++ FubukiMusic/plugins/admins/skip.py
@@ -13,15 +13,15 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 import config
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import YouTube, app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import get_loop
-from YukkiMusic.utils.decorators import AdminRightsCheck
-from YukkiMusic.utils.inline.play import (stream_markup,
+from FubukiMusic import YouTube, app
+from FubukiMusic.core.call import Fubuki
+from FubukiMusic.misc import db
+from FubukiMusic.utils.database import get_loop
+from FubukiMusic.utils.decorators import AdminRightsCheck
+from FubukiMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from YukkiMusic.utils.stream.autoclear import auto_clean
-from YukkiMusic.utils.thumbnails import gen_thumb
+from FubukiMusic.utils.stream.autoclear import auto_clean
+from FubukiMusic.utils.thumbnails import gen_thumb
 
 # Commands
 SKIP_COMMAND = get_command("SKIP_COMMAND")
@@ -69,7 +69,7 @@ async def skip(cli, message: Message, _, chat_id):
                                             message.from_user.first_name
                                         )
                                     )
-                                    await Yukki.stop_stream(chat_id)
+                                    await Fubuki.stop_stream(chat_id)
                                 except:
                                     return
                                 break
@@ -96,7 +96,7 @@ async def skip(cli, message: Message, _, chat_id):
                     _["admin_10"].format(message.from_user.first_name)
                 )
                 try:
-                    return await Yukki.stop_stream(chat_id)
+                    return await Fubuki.stop_stream(chat_id)
                 except:
                     return
         except:
@@ -104,7 +104,7 @@ async def skip(cli, message: Message, _, chat_id):
                 await message.reply_text(
                     _["admin_10"].format(message.from_user.first_name)
                 )
-                return await Yukki.stop_stream(chat_id)
+                return await Fubuki.stop_stream(chat_id)
             except:
                 return
     queued = check[0]["file"]
@@ -120,7 +120,7 @@ async def skip(cli, message: Message, _, chat_id):
                 _["admin_11"].format(title)
             )
         try:
-            await Yukki.skip_stream(chat_id, link, video=status)
+            await Fubuki.skip_stream(chat_id, link, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
         button = telegram_markup(_)
@@ -147,7 +147,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_9"])
         try:
-            await Yukki.skip_stream(chat_id, file_path, video=status)
+            await Fubuki.skip_stream(chat_id, file_path, video=status)
         except Exception:
             return await mystic.edit_text(_["call_9"])
         button = stream_markup(_, videoid)
@@ -163,7 +163,7 @@ async def skip(cli, message: Message, _, chat_id):
         await mystic.delete()
     elif "index_" in queued:
         try:
-            await Yukki.skip_stream(chat_id, videoid, video=True)
+            await Fubuki.skip_stream(chat_id, videoid, video=True)
         except Exception:
             return await message.reply_text(_["call_9"])
         button = telegram_markup(_)
@@ -174,7 +174,7 @@ async def skip(cli, message: Message, _, chat_id):
         )
     else:
         try:
-            await Yukki.skip_stream(chat_id, queued, video=status)
+            await Fubuki.skip_stream(chat_id, queued, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
         if videoid == "telegram":
